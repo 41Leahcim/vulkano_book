b3359665daefa5faddcba51bd6bcab3d57ff6d0c
Michael Scholten <michael-scholten@hotmail.nl>
2023-05-24 09:08:29 +0200

    Initialization - Initialization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use vulkano::{VulkanLibrary, instance::{Instance, InstanceCreateInfo}};
+use vulkano::{
+    instance::{Instance, InstanceCreateInfo},
+    VulkanLibrary,
+};
 
 fn main() {
     let library = VulkanLibrary::new().expect("No local Vulkan library/DLL");
-    let instance = Instance::new(library, InstanceCreateInfo::default())
-        .expect("failed to create instance");
-    
+    let instance =
+        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+
+    for device in instance
+        .enumerate_physical_devices()
+        .expect("Could not enumerate devices")
+    {
+        println!("{}", device.properties().device_name);
+    }
+
+    let physical_devices = instance
+        .enumerate_physical_devices()
+        .expect("Could not enumerate devices")
+        .next()
+        .expect("no devices available");
 }
